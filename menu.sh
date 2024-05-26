abffbb52355c9bd8a134aef7596126cf76120e4b
Jeyson Orozco <jeraoh90@gmail.com>
2024-05-25 21:38:14 -0500

    Create menu.sh
    menu del scrip

---
+++ menu.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Sección de Menú
+echo -e ""
+echo -e "+ -- --=[ El Mundo es Código Abierto ]=-- -- +" | lolcat
+
+echo -e ""
+echo -e ""
+echo -e "Menú"
+echo -e "===="
+echo -e ""
+echo -e "   Comando            Descripción"
+echo -e "   -------            -----------"
+echo -e "   menu               Mostrar todos los comandos disponibles"
+echo -e "   usernew            Crear una nueva cuenta"
+echo -e "   trial              Crear una cuenta de prueba de 24 horas"
+echo -e "   delete             Terminar acceso a la cuenta"
+echo -e "   check              Verificar inicio de sesión de la cuenta"
+echo -e "   member             Listar todos los miembros"
+echo -e "   restart            Reiniciar dropbear, webmin, squid3, OpenVPN y servicios SSH"
+echo -e "   reboot             Reiniciar el servidor virtual"
+echo -e "   speedtest          Realizar prueba de velocidad de ancho de banda del servidor"
+echo -e "   info               Información del sistema"
+echo -e "   about              Información del script automático"
+echo -e "   exit               Salir de la conexión al servidor remoto"
+echo -e ""
+echo -e "Siéntete libre de contribuir y sugerir nuevas características para el script"
